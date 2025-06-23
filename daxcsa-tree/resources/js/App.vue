029e9abe07ef6c6de5573b37c7e4b3d7a4ac7b0e
josue <jocrugo14@gmail.com>
2025-06-23 11:13:39 -0600

    Implementación de vista

---
+++ daxcsa-tree/resources/js/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TreeView from './components/TreeView.vue'
+import data from './assets/Daxcsa.json'
+
+const rootNode = data.data.attributes[0] // Este es el primer distribuidor
+</script>
+
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold mb-4">Árbol Genealógico Daxcsa</h1>
+    <TreeView :node="rootNode" />
+  </div>
+</template>
---
+++ daxcsa-tree/resources/js/components/TreeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>{{ node.username }}</div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  node: Object
+})
+</script>
