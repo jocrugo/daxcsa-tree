ca7542b3a4cc80137ef19d88850b267261b80291
josue <jocrugo14@gmail.com>
2025-06-23 11:47:21 -0600

    Implementación de hijos a la vista principal

--- daxcsa-tree/resources/js/App.vue
+++ daxcsa-tree/resources/js/App.vue
@@ -2,11 +2,11 @@
 import TreeView from './components/TreeView.vue'
 import data from './assets/Daxcsa.json'
 
-const rootNode = data.data.attributes[0] // Este es el primer distribuidor
+const rootNode = data.data.attributes[0] // Primer nodo (distributor.16344)
 </script>
 
 <template>
-  <div class="p-4">
+  <div class="p-6">
     <h1 class="text-2xl font-bold mb-4">Árbol Genealógico Daxcsa</h1>
     <TreeView :node="rootNode" />
   </div>
--- daxcsa-tree/resources/js/components/TreeView.vue
+++ daxcsa-tree/resources/js/components/TreeView.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-    <div>{{ node.username }}</div>
-  </div>
-</template>
-
-<script setup>
-const props = defineProps({
-  node: Object
-})
-</script>
+    <div class="p-4 border rounded shadow bg-white w-fit">
+      <h2 class="font-bold">{{ node.username }}</h2>
+      <p>{{ node.full_name }}</p>
+      <p class="text-sm text-gray-500">{{ node.status }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    node: Object
+  })
+  </script>
+  
