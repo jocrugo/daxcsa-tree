f7d62e9717b3125867cb84f517a96faad65c01f2
josue <jocrugo14@gmail.com>
2025-06-23 16:00:39 -0600

    Implementación de netlify

--- daxcsa-tree/vite.config.js
+++ daxcsa-tree/vite.config.js
@@ -3,26 +3,37 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
         },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
     },
+  },
+  build: {
+    outDir: 'dist',        // <- carpeta de salida
+    emptyOutDir: true,     // <- limpia ./dist antes de generar
+    manifest: true,        // <- genera manifest.json si lo necesitas
+    rollupOptions: {
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+    },
+  },
 });
